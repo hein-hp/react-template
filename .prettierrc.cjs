51f80825da2fed6769b63584d035558cdf798db5
hein.hpq <hein.hpq@gmail.com>
2025-11-29 11:33:55 +0800

    feat: 代码格式化

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: true,
+  trailingComma: "all",
+  htmlWhitespaceSensitivity: "strict",
+  endOfLine: "auto",
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from 'antd'
-import { CiCircleFilled } from '@ant-design/icons'
-import './App.css'
+import { Button } from "antd";
+import { CiCircleFilled } from "@ant-design/icons";
 
 function App() {
   return (
-    <div className='justify-center items-center h-screen flex'>
-      <Button type="primary" className='flex items-center gap-2'>
-        <CiCircleFilled size={16} /> 
+    <div className="justify-center items-center h-screen flex">
+      <Button type="primary" className="flex items-center gap-2">
+        <CiCircleFilled size={16} />
         Hello world with Ant Design + Tailwind CSS!
       </Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
